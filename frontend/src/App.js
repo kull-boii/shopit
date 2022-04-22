88c26e73e333011863f17f9fcce9e552969d5157
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-04-22 09:43:07 +0530

    skeleton structure created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/">ShopIt</a>
       </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="prodcut-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    {
+      name: "Nike slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image:
+        "https://raw.githubusercontent.com/basir/mern-amazona/master/frontend/public/images/p1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Adidas slim shirt",
+      slug: "adidas-slim-shirt",
+      category: "Shirts",
+      image:
+        "https://raw.githubusercontent.com/basir/mern-amazona/master/frontend/public/images/p2.jpg",
+      price: 320,
+      countInStock: 10,
+      brand: "Adidas",
+      rating: 4,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike slim pant",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image:
+        "https://raw.githubusercontent.com/basir/mern-amazona/master/frontend/public/images/p3.jpg",
+      price: 240,
+      countInStock: 5,
+      brand: "Nike",
+      rating: 5,
+      numReviews: 10,
+      description: "high quality pant",
+    },
+  ],
+};
+
+export default data;
